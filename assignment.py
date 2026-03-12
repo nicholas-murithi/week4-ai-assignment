72f293d88de2af7222bc3cf71330624efa0d0532
Nicholas Murithi <nicholas960murithi@gmail.com>
2026-03-12 23:21:36 +0300

    Add assignment.py with questions and outputs

---
+++ assignment.py
@@ -0,0 +1,20 @@
+# Question 4
+ print("Hello AI Students")
+
+# Question 5
+Name = "John"
+Age = 20
+City = "Nairobi"
+print(f"Name:{Name},Age:{Age},City:{City}")
+
+# Question 6
+programming_languagelist = ["Python","Java", "C++","Javascript","Go"]
+print(programming_languagelist[0])
+
+# Question 7
+student ={
+    "name":"John",
+    "age":21,
+    "course":"AI"
+}
+print(student["name"])
